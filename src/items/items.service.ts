1985c6695eec36bd172fc694afef5fea2d411b8f
crack <crack>
2019-05-02 00:49:22 +0900

    create items module, Item interface & fake data in service

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: "343434",
+      name: "Item One",
+      qty: 100,
+      description: "This is item one"
+    },
+    {
+      id: "98495",
+      name: "Item Two",
+      qty: 50,
+      description: "This is item two"
+    },
+  ];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+}
