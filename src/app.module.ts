648ff7ea970892ba97ddb641d577291eafc93b63
crack <crack>
2019-05-07 16:44:32 +0900

    database connection config change : create ormconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,9 @@ import { TodosController } from './todos/todos.controller';
 import { TodosService } from './todos/todos.service';
 import { TodosModule } from './todos/todos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 
 @Module({
-  imports: [TodosModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: '104.198.118.66',
-    port: 3306,
-    username: 'root',
-    password: '1234',
-    database: 'test',
-    entities: [join(__dirname, './todos/entity/*.ts')],
-    synchronize: true,
-  })],
+  imports: [TodosModule, TypeOrmModule.forRoot()],
   controllers: [AppController, TodosController],
   providers: [AppService, TodosService],
 })
