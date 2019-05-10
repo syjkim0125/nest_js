7b9db1c729bbbcb971d143d206dfc08744027599
crack <crack>
2019-05-10 17:24:21 +0900

    change DTO : interface -> class & add class-validator

--- src/todos/dto/todo.dto.ts
+++ src/todos/dto/todo.dto.ts
@@ -1,5 +1,12 @@
-export interface TodoDTO {
+import { IsString, IsDate } from 'class-validator';
+
+export class TodoDTO {
+  @IsString()
   title: string;
+
+  @IsString()
   content: string;
+
+  @IsDate()
   due_date: Date;
 }
